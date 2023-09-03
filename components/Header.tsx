ecd74a56f463574b6ec9e901b785f0fff849a280
jisilver <jisilver.kim@gmail.com>
2023-09-03 16:14:47 +0900

    Feat: 구글, 카카오 프로필 사진 등록

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,27 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Image from 'next/image'
 import { useSession, signOut } from "next-auth/react"
 import CustomButton from './CustomButton'
 import { HiPencilSquare, HiOutlineArrowLeftOnRectangle } from "react-icons/hi2";
 import { useRouter } from 'next/navigation'
-const USER_IMAGE = "https://res.cloudinary.com/jisilver/image/upload/v1690546283/upload/l9wb59xf1luissmkmpso.png"
+const USER_IMAGE = "https://res.cloudinary.com/jisilver/image/upload/v1693725189/upload/wcystucfzjmot8dbqv3l.png"
 
 const Header = () => {
     const { data: session } = useSession();
     const router = useRouter();
 
+    useEffect(() => {
+        console.log(session?.user?.image)
+    }, [session])
+
     const handleButtonClick = () => {
         if (session) {
-            signOut({ callbackUrl: "/" });
+            if (window.confirm('로그아웃 하시겠습니까?')) {
+                signOut({ callbackUrl: "/" });
+            } else {
+                return;
+            }
         } else {
             router.push('/login')
         }
@@ -25,7 +33,9 @@ const Header = () => {
             <div className='flex gap-4'>
                 <CustomButton containerStyles='bg-black text-white rounded-full flex items-center' title="글쓰기" icon textStyles='hidden sm:block'><HiPencilSquare /></CustomButton>
                 <CustomButton containerStyles="text-gray-500 border-[1px] rounded-full flex items-center" title={session ? "로그아웃" : "로그인"} handleClick={handleButtonClick} icon textStyles='hidden sm:block'><HiOutlineArrowLeftOnRectangle /></CustomButton>
-                <Image src={USER_IMAGE} width={40} height={40} alt="user image" className='rounded-full' />
+                <div className='w-[40px] h-[40px] relative'>
+                    <Image src={session?.user?.image || USER_IMAGE} fill alt="user image" className='rounded-full' />
+                </div>
             </div>
         </div>
     )
