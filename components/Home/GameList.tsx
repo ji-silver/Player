2bcb8982d968d3abd733e2d97b69f7e66c302732
jisilver <jisilver.kim@gmail.com>
2023-09-03 23:30:16 +0900

    Design: gameList 화면 배치

--- components/Home/GameList.tsx
+++ components/Home/GameList.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+import { gameList } from '@/constants'
+import Image from 'next/image'
 
 const GameList = () => {
     return (
-        <div>
-            GameList
+        <div className='grid grid-cols-3 md:grid-cols-6 my-7 gap-5'>
+            {gameList.map((item) => (
+                <div className='gameList__container'>
+                    <Image src={item.image} alt="sports" width={45} height={45} className='hover:animate-bounce transition-all duration-150' />
+                    <h3 className='gameList__name'>{item.name}</h3>
+                </div>
+            ))}
         </div>
     )
 }
---
+++ constants/index.ts
@@ -0,0 +1,32 @@
+export const gameList = [
+  {
+    id: 1,
+    name: "축구",
+    image: "/images/soccer.png",
+  },
+  {
+    id: 2,
+    name: "배드민턴",
+    image: "/images/badminton.png",
+  },
+  {
+    id: 3,
+    name: "볼링",
+    image: "/images/bowling.png",
+  },
+  {
+    id: 4,
+    name: "골프",
+    image: "/images/golf.png",
+  },
+  {
+    id: 5,
+    name: "탁구",
+    image: "/images/ping-pong.png",
+  },
+  {
+    id: 6,
+    name: "그 외",
+    image: "/images/bike.png",
+  },
+];
